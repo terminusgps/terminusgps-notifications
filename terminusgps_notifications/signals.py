9b1ede4c5bb46e870608fae394f6557897b0e1af
darthnall <blake@proton.me>
2025-10-23 11:44:15 -0500

    Improved signals

--- terminusgps_notifications/signals.py
+++ terminusgps_notifications/signals.py
@@ -11,35 +11,20 @@ def create_notification_resource_in_wialon(sender, **kwargs):
     if customer := kwargs.get("instance"):
         if not customer.resource_id and hasattr(customer, "token"):
             token = getattr(customer, "token").name
-            with WialonSession(token=token) as session:
-                try:
-                    logging.debug(
-                        f"Searching Wialon for notification resource on behalf of '{customer}'..."
-                    )
+            try:
+                with WialonSession(token=token) as session:
                     results = services.search_wialon_for_notification_resource(
                         session
                     )
-                    if results["totalItemsCount"] == 1:
+                    if results["totalItemsCount"] > 0:
                         customer.resource_id = int(results["items"][0]["id"])
-                        logging.info(
-                            f"Found existing notification resource for '{customer}': '{customer.resource_id}'"
-                        )
                     else:
-                        logging.debug(
-                            f"No notification resource found for: '{customer}'..."
-                        )
-                        logging.info(
-                            f"Creating notification resource on behalf of '{customer}'..."
-                        )
                         response = (
                             services.create_wialon_notification_resource(
                                 session
                             )
                         )
                         customer.resource_id = int(response["item"]["id"])
-                        logging.debug(
-                            f"Created notification resource for '{customer}': '{customer.resource_id}'"
-                        )
                     customer.save(update_fields=["resource_id"])
-                except WialonAPIError as e:
-                    logger.warning(e)
+            except WialonAPIError as e:
+                logger.critical(e)
